d9e4f96f5ce996ee9e9de880dd0084b0baa5a411
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-05-30 12:14:13 +0530

    Add files via upload

--- ClassExtensionTest.java
+++ ClassExtensionTest.java
@@ -1,13 +1,63 @@
 
 public class ClassExtensionTest {
 	public static void main(String[] args) {
+		
 		Doctor x = new Doctor();
 		x.diagnose(); // early binding - x.Doctor.diagnose();
 		
-		Surgeon s = new Surgeon();
+		if(x instanceof Doctor) {
+			System.out.println("YES, x is pointing at Doctor...");
+		}
+		else {
+			System.out.println("No, x is NOT pointing at Doctor...");
+		}
+		
+		System.out.println("---------------");
+		
+		Surgeon s = new Surgeon(); //A Surgeon is also a Doctor
 		s.diagnose(); // early binding - s.Surgeon.diagnose();
 		
+		if(s instanceof Doctor) {
+			System.out.println("YES, s is pointing at Doctor...");
+		}
+		else { 
+			System.out.println("No, s is NOT pointing at Doctor...");
+		}
+		
+		if(s instanceof Surgeon) {
+			System.out.println("YES, s is pointing at Surgeon...");
+		}
+		else { 
+			System.out.println("No, s is NOT pointing at Surgeon...");
+		}
+		System.out.println("--------------------------");
+
+		
 		HeartSurgeon hs = new HeartSurgeon();
+		
+		if(hs instanceof Doctor) {
+			System.out.println("YES, hs is pointing at Doctor...");
+		}
+		else { 
+			System.out.println("No, hs is NOT pointing at Doctor...");
+		}
+		
+		if(hs instanceof Surgeon) {
+			System.out.println("YES, hs is pointing at Surgeon...");
+		}
+		else { 
+			System.out.println("No, hs is NOT pointing at Surgeon...");
+		}
+		
+		if(hs instanceof HeartSurgeon) {
+			System.out.println("YES, hs is pointing at HeartSurgeon...");
+		}
+		else { 
+			System.out.println("No, hs is NOT pointing at HeartSurgeon...");
+		}
+		
+		
+		
 		hs.diagnose(); // early binding -  hs.HeartSurgeon.diagnose();
 		
 		System.out.println("--------------------------");
@@ -26,6 +76,9 @@ public class ClassExtensionTest {
 		
 		d.diagnose(); // d.Doctor.diagnose(); - COMPILE TIME - EARLY
 						//d.HeartSurgeon.diagnose() - RUNTIME - LATE
+		System.out.println("--------------------------------");
+		
+		
 	}
 }
 class Doctor {
@@ -34,12 +87,12 @@ class Doctor {
 	}
 }
 class Surgeon extends Doctor { //isA
-	/*void diagnose() { //overriding by the child class
+	void diagnose() { //overriding by the child class
 		System.out.println("Surgeon:diagnose() called...CT scan...");
-	}*/
+	}
 }
 class HeartSurgeon extends Surgeon { //isA
-	/*void diagnose() { //overriding by the child class
+	void diagnose() { //overriding by the child class
 		System.out.println("HeartSurgeon:diagnose() called...ECG scan...");
-	}*/
+	}
 }
