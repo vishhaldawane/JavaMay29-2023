5c84871e25aa5173971f7c78b892a03066f53bb7
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-05-31 15:41:03 +0530

    Add files via upload

--- DivideTest.java
+++ DivideTest.java
@@ -1,10 +1,52 @@
 
 public class DivideTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+		
+		System.out.println("BEGIN");
+		
+		int ary[]= {10,20,30};
+		System.out.println("array "+ary[2]);
+		
+		String str="sbi";
+		System.out.println("string 5th letter : "+str.toUpperCase());
+		
+		String str2="state bank of india";
+		System.out.println("string 15th letter : "+str.charAt(15));
+		
 		Calculator calc = new Calculator();
-		calc.divide(10, 0);
-		calc.divide(10, 3);
-		calc.divide(10, 2);
+		try {
+			calc.divide(10, 0); //3 - here it is uncaught - it is rethrown to JVM - which drops it by terminating ur method here
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Problem1 : "+e);
+		}
+		catch(NullPointerException e) {
+			System.out.println("Problem2 : "+e);
+		}
+		catch(RuntimeException e) {
+			System.out.println("Problem3 : "+e);
+		}
+		catch(Exception e) {
+			System.out.println("Problem4 : "+e);
+		}
+		
+		
+		
+		
+		
+		try {
+			calc.divide(20, 0);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Problem : "+e);
+		}
+		
+		try {
+			calc.divide(30, 0);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Problem : "+e);
+		}
 	}
 }
 class Calculator
@@ -14,13 +56,9 @@ class Calculator
 		System.out.println("Numerator  : "+num);
 		System.out.println("denominator: "+deno);
 		System.out.println("Trying to divide "+num+" by "+deno);
-		if(deno !=0) {
-			int div = num / deno;
-			System.out.println("Division   : "+div);
-		}
-		else {
-			System.out.println("Cannot divide by ZERO");
-		}
+		int div = num / deno; // --> JVM --1--> CPU
+							  //    --2--> throw new ArithmeticException("/ by zero");
+		System.out.println("Division   : "+div);
 		System.out.println("END OF DIVISION\n");
 	}
 }
