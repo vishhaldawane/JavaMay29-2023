e671c58b8894d7734132c5a12e32fd18b8d39909
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-06-02 12:06:50 +0530

    Add files via upload

---
+++ TreeSetTest2.java
@@ -0,0 +1,96 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+/*
+ * 						8
+ * 						|
+ * 					----------------
+ * 					|			|
+ * 					2			
+ * 					|
+ * 				--------------
+ * 				|		|
+ * 				1		
+ * 
+ * 						2
+ * 						|
+ * 				---------------------
+ * 				|				|
+ * 				1				8
+ * 								|
+ * 							-----------------
+ * 							|			|
+ * 							3			14
+ * 
+ * 
+ */
+public class TreeSetTest2 {
+	public static void main(String[] args) {
+		ChemicalElement element1 = new ChemicalElement(8, "Oxygen", "O", 15.999f);
+		ChemicalElement element2 = new ChemicalElement(2, "Helium", "He", 4.0026f);
+		ChemicalElement element3 = new ChemicalElement(1, "Hydrogen", "H", 1.008f);
+		ChemicalElement element4 = new ChemicalElement(14, "Silicon", "Si", 28.085f);
+		ChemicalElement element5 = new ChemicalElement(3, "Lithium", "Li", 6.94f);
+		
+		System.out.println("Elements are created....");
+		
+		TreeSet<ChemicalElement> periodicTableSet = new TreeSet<ChemicalElement>();
+		System.out.println("Empty Periodic Table is created....");
+		
+		System.out.println("NOW ..Adding the first element...");
+		periodicTableSet.add(element1);  
+		
+		System.out.println("NOW ..Adding the second element...");
+		periodicTableSet.add(element2);  
+		
+		System.out.println("NOW ..Adding the  third element...");
+		periodicTableSet.add(element3);   
+		
+		System.out.println("NOW ..Adding the fourth element...");
+		periodicTableSet.add(element4);  
+		
+		System.out.println("NOW ..Adding the fifth element...");
+		periodicTableSet.add(element5);  
+		
+		System.out.println("Added all the elements...");
+		
+		Iterator<ChemicalElement> elementIterator =  periodicTableSet.iterator();
+		while(elementIterator.hasNext()) {
+			ChemicalElement theElement = elementIterator.next();
+			System.out.println("Element : "+theElement );
+		}
+	}
+}
+
+class ChemicalElement implements Comparable<ChemicalElement>
+{
+	int atomicNumber;
+	String atomicName;
+	String atomicFormula;
+	float atomicWeight;
+
+	
+	public ChemicalElement(int atomicNumber, String atomicName, String atomicFormula, float atomicWeight) {
+		super();
+		this.atomicNumber = atomicNumber;
+		this.atomicName = atomicName;
+		this.atomicFormula = atomicFormula;
+		this.atomicWeight = atomicWeight;
+	}
+	@Override
+	public String toString() {
+		return "ChemicalElement [atomicNumber=" + atomicNumber + ", atomicName=" + atomicName + ", atomicFormula="
+				+ atomicFormula + ", atomicWeight=" + atomicWeight + "]";
+	}
+	/*@Override
+	public int compareTo(ChemicalElement o) {
+		System.out.println("Comparing "+atomicNumber+" with "+o.atomicNumber);
+		return Integer.compare(atomicNumber, o.atomicNumber);
+		
+	}*/
+	public int compareTo(ChemicalElement o) {
+		System.out.println("Comparing "+atomicName+" with "+o.atomicName);
+		return atomicName.compareTo(o.atomicName);
+		
+	}
+	
+}
